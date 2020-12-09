0ca0b1e6685da561fc038395e973dc322423a182
JonnathanEspinoza <jonnathan.espinoza684j@gmail.com>
2020-12-08 23:29:33 -0500

    creacion de los modulos de remove Category y find by Id Category

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -1,21 +1,47 @@
 const Category = require('../models/Category');
 const { errorHandler } = require('../helpers/dberrorHandler');
 
-exports.create = async(req, res) => {
+// Crear una categoria
+exports.create = async (req, res) => {
     const category = new Category(req.body);
     await category.save((err, data) => {
         if (err) {
-            return res.status(400).json({error: errorHandler(err)});
+            return res.status(400).json({ error: errorHandler(err) });
         }
-        res.json({data});
+        res.json({ data });
     });
 };
 
-exports.list = async(req, res) => {
-    const category = new Category.find().exec((err, data) => {
+// Listar todas las categorias
+exports.list = async (req, res) => {
+    const category = Category.find().exec((err, data) => {
         if (err) {
-            return res.status(400).json({error: errorHandler(err)});
+            return res.status(400).json({ error: errorHandler(err) });
         }
         res.json(data);
+    });
+}
+
+// Eliminar una categoria
+exports.remove = (req, res) => {
+    let category = req.category;
+    category.remove((err, data) => {
+        if (err) {
+            return res.status(400).json({ error: errorHandler(err) })
+        }
+        res.json({
+            message: "Category successfully deleted"
+        })
+    });
+}
+
+// Buscar categoria por id
+exports.categoryById = async(req, res, next, id) => {
+    await Category.findById(id).exec( (err, data) => {
+        if (err || !data) {
+            return res.status(400).json({ error: "Category was not found or does not exist"});
+        }
+        req.category = data;
+        next();
     });
 }
--- src/routes/controller.routes.js
+++ src/routes/controller.routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { list, create } = require('../controllers/category.controller');
+const { list, create, remove, categoryById } = require('../controllers/category.controller');
 
 router.get('/categories', list);
-router.post('/crate', create);
+router.post('/create', create);
+router.delete('/:categoryId', remove);
+
+router.param('categoryId', categoryById);
 
 module.exports=router;
